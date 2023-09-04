b0e40bb6e39d3f26a30e2a4b3e4e5425aefeb354
Sergey Burdin <senik1806@gmail.com>
2023-09-04 17:29:43 +0300

    First

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ const qrcode = require('qrcode-terminal');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
+const admin = '79108257989@c.us'
+const igor = '79611601191@c.us'
+const katya = '79301200905@c.us'
+
+function getStopWords() {
+    return fs.readFileSync('./test.txt', 'utf8').toString().split('\n')
+}
+
+function checkWordIsStop(message) {
+    console.log('checkWordIsStop', message)
+    const stop_words = getStopWords()
+    console.log('checkWordIsStop', stop_words)
+    let no_stop = true
+    stop_words.forEach((word, index) => {
+        no_stop = no_stop && !message.toLowerCase().includes(word)
+        console.log('check word ', word, no_stop)
+    });
+    return !no_stop
+}
+
+function addNewStopWord(new_word) {
+    fs.appendFile('./test.txt', '\n' + new_word.toLowerCase(), (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log('success add new word: ', new_word)
+    })
+}
+
 const client = new Client(
     {
         authStrategy: new LocalAuth(),
@@ -20,6 +50,50 @@ client.on('ready', () => {
 
 client.on('message', message => {
     console.log('message', message)
+
+    let is_message = true
+    if (message.from === admin) {
+        if (message.body.length > 3) {
+            if (message.body.slice(0, 3) === '*7#') {
+                is_message = false
+                const new_word = message.body.slice(3)
+                addNewStopWord(new_word)
+            }
+        }
+    }
+
+    
+    if (is_message) {
+        if (message.from === katya) {
+            if (checkWordIsStop(message.body)) {
+                client.sendMessage(admin, 'ATTENTION!!! stop word from Katya in message: ' + message.body);
+            }
+            else {
+                let mess = 'Katya: ' + message.body
+                client.sendMessage(igor, mess);
+
+                client.sendMessage('79884054121@c.us', mess);
+            }
+
+        }
+        else if (message.from === igor) {
+            if (checkWordIsStop(message.body)) {
+                client.sendMessage(admin, 'ATTENTION!!! stop word from Igor in message: ', message.body);
+            }
+            else {
+                let mess = 'Igor: ' + message.body
+                client.sendMessage(katya, mess);
+
+                client.sendMessage('79884054121@c.us', mess);
+            }
+        }
+        // else if (message.from === '79884054121@c.us') {
+        //     let mess = 'Admin: ' + message.body
+        //     client.sendMessage('79159975804@c.us', mess);
+        //     client.sendMessage('79611601191@c.us', mess);
+        //     client.sendMessage('79504460593@c.us', mess);
+        // }
+    }
 });
 
 client.initialize();
