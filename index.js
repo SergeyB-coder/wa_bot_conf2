e4a87ceb0865ff7969b4d284c12ba1f72a2641cf
Sergey Burdin <senik1806@gmail.com>
2023-09-23 13:04:36 +0300

    2

--- index.js
+++ index.js
@@ -1,101 +1,146 @@
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-const TOKEN_PATH = 'token.json';
+const qrcode = require('qrcode-terminal');
 
-function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err)
-          return console.error(
-            'Error while trying to retrieve access token',
-            err
-          );
-        oAuth2Client.setCredentials(token);
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) return console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
-        });
-        callback(oAuth2Client);
-      });
-    });
-  }
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-function authorize(credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id,
-        client_secret,
-        redirect_uris[0]
-    );
+var sheetdb = require('sheetdb-node');
 
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    });
-}
+// create a config file
+var config = {
+    address: 'fdvo0jh0vaqar',
+};
 
-fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    authorize(JSON.parse(content), (d) => {
+// Create new client
+var client = sheetdb(config);
 
-    });
+const question_name = `Привет, у меня есть крытые вагоны разной кубатуры в РФ и КЗХ.
+Как Вас зовут?`
+
+const question_company = `Из какой Вы компании?`
+
+const question_role = `Вы Грузоотправитель? Экспедитор? Трейдер? Грузополучатель?`
+
+const question_price = `Ваши маршруты, ценовые условия?`
+
+const question_conditions = `Условия по ПРР?`
+
+const message_thank = `Спасибо, мы обязательно свяжемся с Вами после выставки.
+Напишите электронную почту.`
+
+
+
+
+const clientwa = new Client(
+    {
+        authStrategy: new LocalAuth(),
+        // puppeteer: {
+        //     headless: true,
+        //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        //     executablePath: '/usr/bin/google-chrome-stable'
+        // }
+    }
+);
+
+clientwa.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
 });
 
+clientwa.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+clientwa.on('message', message => {
+    console.log('message', message)
+    const phone = message.from
+    //find phone
+    client.read({ search: { phone: phone } }).then(function (data) {
+        const data_json = JSON.parse(data)
+        console.log('search phone: ', data_json, data_json.length);
+        if (data_json.length === 0) {
+            client.create({ phone: phone }).then(function (data) {
+                console.log('create', data);
+
+                clientwa.sendMessage(message.from, question_name)
+
+            }, function (err) {
+                console.log(err);
+            });
+        }
+        else if (data_json[0].name === '') {
+            client.update(
+                'phone', // column name
+                phone, // value to search for
+                { 'name': message.body } // object with updates
+              ).then(function(data) {
+                console.log('update name', data);
+
+                clientwa.sendMessage(message.from, question_role)
+
+              }, function(err){
+                console.log(err);
+              });
+        }
+        else if (data_json[0].role === '') {
+            client.update(
+                'phone', // column name
+                phone, // value to search for
+                { 'role': message.body } // object with updates
+              ).then(function(data) {
+                console.log('update role', data);
+
+                clientwa.sendMessage(message.from, question_price)
+
+              }, function(err){
+                console.log(err);
+              });
+        }
+        else if (data_json[0].price === '') {
+            client.update(
+                'phone', // column name
+                phone, // value to search for
+                { 'price': message.body } // object with updates
+              ).then(function(data) {
+                console.log('update price', data);
+
+                clientwa.sendMessage(message.from, question_conditions)
+
+              }, function(err){
+                console.log(err);
+              });
+        }
+        else if (data_json[0].conditions === '') {
+            client.update(
+                'phone', // column name
+                phone, // value to search for
+                { 'conditions': message.body } // object with updates
+              ).then(function(data) {
+                console.log('update conditions', data);
+
+                clientwa.sendMessage(message.from, message_thank)
+
+              }, function(err){
+                console.log(err);
+              });
+        }
+        else if (data_json[0].email === '') {
+            client.update(
+                'phone', // column name
+                phone, // value to search for
+                { 'email': message.body } // object with updates
+              ).then(function(data) {
+                console.log('update email', data);
+
+                // clientwa.sendMessage(message.from, message_thank)
+
+              }, function(err){
+                console.log(err);
+              });
+        }
+    }, function (err) {
+        console.log(err);
+    });
 
 
+    // clientwa.sendMessage(message.from, question_name)
+});
 
-
-
-
-// const qrcode = require('qrcode-terminal');
-
-// const { Client, LocalAuth } = require('whatsapp-web.js');
-
-
-// // const admin = '79024050778@c.us'
-
-
-// const client = new Client(
-//     {
-//         authStrategy: new LocalAuth(),
-//         puppeteer: { 
-//             headless: true, 
-//             args: ['--no-sandbox', '--disable-setuid-sandbox'],
-//             // executablePath: '/usr/bin/google-chrome-stable'
-//         }
-//     }
-// );
-
-
-
-// client.on('qr', qr => {
-//     qrcode.generate(qr, { small: true });
-// });
-
-// client.on('ready', () => {
-//     console.log('Client is ready!');
-// });
-
-// client.on('message', message => {
-//     console.log('message', message)
-
-
-// });
-
-// // v1 
-// client.initialize();
-
-console.log(9)
+clientwa.initialize();
